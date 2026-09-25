58ca6879cc171cdec30f9734f855ddfdd4748b61
eczbeytullahsimsek-crypto <eczbeytullahsimsek@gmail.com>
2026-09-26 00:40:36 +0300

    Add Java MainActivity

---
+++ app/src/main/java/com/smoothnotes/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smoothnotes.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        DrawingView drawingView = new DrawingView(this);
+        setContentView(drawingView);
+    }
+}
