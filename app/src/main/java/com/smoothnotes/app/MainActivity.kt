f30fbb2a4c830adcd69bad49ca8e3802db4eb9b9
eczbeytullahsimsek-crypto <eczbeytullahsimsek@gmail.com>
2026-09-26 00:32:29 +0300

    Fix app startup crash

--- app/src/main/java/com/smoothnotes/app/MainActivity.kt
+++ app/src/main/java/com/smoothnotes/app/MainActivity.kt
@@ -2,21 +2,12 @@ package com.smoothnotes.app
 
 import android.app.Activity
 import android.os.Bundle
-import android.graphics.Color
-import android.view.Window
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-        window.statusBarColor = Color.WHITE
-        window.navigationBarColor = Color.WHITE
-
-        val drawingView = DrawingView(this)
-
-        setContentView(drawingView)
+        setContentView(DrawingView(this))
     }
 }
